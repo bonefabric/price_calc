75e6dcd3c5f3cb514d6b59aa21cf91cd4e32aa5e
Insaf <88691128+bonefabric@users.noreply.github.com>
2024-12-06 13:09:11 +0500

    Update yandex-eda-price.user.js

--- yandex-eda-price.user.js
+++ yandex-eda-price.user.js
@@ -1,3 +1,18 @@
+// ==UserScript==
+// @name        Yandex Eda price calculator
+// @namespace   yandex-eda-calc
+// @match       *://eda.yandex.ru/*
+// @grant       none
+// @version     1.5
+// @author      Insaf Burangulov
+// @description Калькулятор цен яндекс еды
+// @license     MIT
+// @updateURL    https://raw.githubusercontent.com/bonefabric/price_calc/main/yandex-eda-price.meta.js
+// @downloadURL  https://raw.githubusercontent.com/bonefabric/price_calc/main/yandex-eda-price.user.js
+// @update       https://raw.githubusercontent.com/bonefabric/price_calc/main/yandex-eda-price.user.js
+// @run-at       document-end
+// ==/UserScript==
+
 // Главный селектор карточки
 const CARD_PRICE_ROOT_CLASS = 'UiKitDesktopProductCard_descriptionWrapper';
 

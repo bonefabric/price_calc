96aabfc9ba94bde820ee8a6e9f6211ddd750785c
Insaf <88691128+bonefabric@users.noreply.github.com>
2024-12-06 13:16:21 +0500

    Update yandex-eda-price.meta.js

--- yandex-eda-price.meta.js
+++ yandex-eda-price.meta.js
@@ -7,9 +7,8 @@
 // @author      Insaf Burangulov
 // @description Калькулятор цен яндекс еды
 // @license     MIT
-// @updateURL    https://raw.githubusercontent.com/bonefabric/price_calc/main/app.js
-// @downloadURL  https://raw.githubusercontent.com/bonefabric/price_calc/main/app.js
-// @update       https://raw.githubusercontent.com/bonefabric/price_calc/main/app.js
+// @updateURL    https://raw.githubusercontent.com/bonefabric/price_calc/main/yandex-eda-price.meta.js
+// @downloadURL  https://raw.githubusercontent.com/bonefabric/price_calc/main/yandex-eda-price.user.js
+// @update       https://raw.githubusercontent.com/bonefabric/price_calc/main/yandex-eda-price.user.js
 // @run-at       document-end
 // ==/UserScript==
-

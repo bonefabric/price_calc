e14b1e5d60ab7d9b5f07d292683cedfd93958898
Insaf <88691128+bonefabric@users.noreply.github.com>
2024-12-06 13:06:01 +0500

    Update and rename app.js to yandex-eda-price.meta.js

---
+++ yandex-eda-price.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Yandex Eda price calculator
+// @namespace   yandex-eda-calc
+// @match       *://eda.yandex.ru/*
+// @grant       none
+// @version     1.5
+// @author      Insaf Burangulov
+// @description Калькулятор цен яндекс еды
+// @license     MIT
+// @updateURL    https://raw.githubusercontent.com/bonefabric/price_calc/main/app.js
+// @downloadURL  https://raw.githubusercontent.com/bonefabric/price_calc/main/app.js
+// @update       https://raw.githubusercontent.com/bonefabric/price_calc/main/app.js
+// @run-at       document-end
+// ==/UserScript==
+
